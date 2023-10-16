e6633c1bbd1f1fc65feea24c4f5f5c9834d49c4e
Vincent Chann <>
2023-10-16 18:00:13 +0200

    v1 : sync business

--- src/components/BusinessList.vue
+++ src/components/BusinessList.vue
@@ -4,16 +4,43 @@
       LOADING
     </div>
     <div v-else>
+
+      <button style="margin:10px; padding:5px; width: 300px;" @click="syncAllBusinesses()">
+        synchronise all businesses
+      </button>
+      <table style="margin: auto; margin-top:10px;">
+        <thead>
+          <tr>
+            <th style="width:400px">Business</th>
+            <th style="width:100px">Updated</th>
+            <th style="width:100px">Date</th>
+            <th style="width:100px">Synchronise</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="b in business.businesses"
+            :key="b.pk"
+          >
+            <td>
+              <router-link :to="'/detail/'+ b.pk + '/'">
+                {{ b.name }} [{{ b.symbol }}]
+              </router-link>
+            </td>
+            <td :style="b.is_updated ? 'background-color:green' : 'background-color:red'"></td>
+            <td>{{ b.updated }}</td>
+            <td>
+              <button @click="syncBusiness(b.pk)">
+                synchronise
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <div
-        v-for="b in business.businesses"
-        :key="b.pk"
       >
-        <router-link :to="'/detail/'+ b.pk + '/'">
-          {{ b.name }}
-          {{ b.symbol }}
-          {{ b.country_code }}
-        </router-link>
       </div>
+
     </div>
   </div>
 </template>
@@ -24,13 +51,49 @@ export default {
   name: 'BusinessList',
   data () {
     return {
-      business: null
+      business: null,
+      status: null // unused
     }
   },
   mounted () {
-    axios
-      .get('https://04qeb8it0j.execute-api.eu-west-3.amazonaws.com/api/business/')
-      .then(data => { this.business = data.data })
+    this.businesses()
+  },
+  methods: {
+    businesses () {
+      axios
+        .get('https://04qeb8it0j.execute-api.eu-west-3.amazonaws.com/business/')
+        .then(data => { this.business = data.data })
+    },
+    syncAllBusinesses () {
+      this.business = null
+      axios
+        .get('https://04qeb8it0j.execute-api.eu-west-3.amazonaws.com/sync_business/')
+        .then(data => {
+          this.status = data.status
+          this.businesses()
+        })
+    },
+    syncBusiness (businessId) {
+      this.business = null
+      axios
+        .get(`https://04qeb8it0j.execute-api.eu-west-3.amazonaws.com/integration/sync_business/${businessId}`)
+        .then(data => {
+          this.status = data.status
+          this.businesses()
+        })
+    }
   }
 }
 </script>
+<style scoped>
+table,
+td {
+  border: 1px solid #333;
+}
+
+thead,
+tfoot {
+  background-color: #333;
+  color: #fff;
+}
+</style>
